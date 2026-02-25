2a1fe683220f229b940f10500bf68c850bf317e0
it-is-Abhishek <abhishekverma1489@gmail.com>
2026-02-25 12:50:39 +0530

    feat: initialize routing and fix BrowserRouter issue causing blank page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import BuyCredit from './pages/buyCredit';
+import Result from "./pages/Result";
+
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+
+      <Routes>
+        <Route path = '/' element = {<Home/>} />
+        <Route path = '/result' element = {<Result/>} />
+        <Route path = '/buy' element = {<BuyCredit/>} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App
