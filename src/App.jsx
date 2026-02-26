0d17f2fcafedb4e9bdf51648115b02a55be78aad
it-is-Abhishek <abhishekverma1489@gmail.com>
2026-02-26 10:45:33 +0530

    feat: implement Header section with upload button and responsive layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import BuyCredit from './pages/buyCredit';
 import Result from "./pages/Result";
+import Navbar from './components/Navbar';
 
 
 const App = () => {
   return (
-    <div>
-
+    <div className='min-h-screen bg-slate-50'>
+      <Navbar />
       <Routes>
         <Route path = '/' element = {<Home/>} />
         <Route path = '/result' element = {<Result/>} />
---
+++ src/components/Steps.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Steps = () => {
+  return (
+    <div>
+        <h1> Steps to remove background <br/> image in Seconds</h1>
+    </div>
+  )
+}
+
+export default Steps
