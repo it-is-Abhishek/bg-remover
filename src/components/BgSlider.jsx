6a623197bc22ada5166772e680293ce29b49ce2a
it-is-Abhishek <abhishekverma1489@gmail.com>
2026-03-01 18:31:49 +0530

    feat: implement image before/after slider

---
+++ src/components/BgSlider.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
+
+const BgSlider = () => {
+
+    const [sliderPosition, setSliderPosition] = useState(50)
+
+    const handleSliderChange = (e) => {
+        setSliderPosition(e.target.value)
+    }
+
+
+  return (
+    <div>
+        {/* Title */}
+        <h1 className='text-center text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text text-transparent'>
+            Remove Background With High <br/> Quality and Accuracy
+        </h1>
+
+        <div>
+            {/* {Backgroundimage} */}
+            <img src={assets.image_w_bg} style = {{clipPath:`inset(0 ${100.2 - sliderPosition}% 0 0)`}} alt = ""/>
+
+
+            {/* { For ground image} */}
+            <img src = {assets.image_wo_bg} style = {{clipPath:`inset(0 0 0 ${sliderPosition}%)`}} alt = ""/>
+        </div>
+
+    </div>
+  )
+}
+
+export default BgSlider
