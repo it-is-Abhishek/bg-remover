f89d5640893bd1aa97fd9a67f5f356b531320416
it-is-Abhishek <abhishekverma1489@gmail.com>
2026-04-17 19:44:46 +0530

    feat: Added Webhooks

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
+import userRouter from './routes/userRoutes.js'
 
 //API CONFIG
 const PORT = process.env.PORT || 4000
@@ -31,6 +32,7 @@ app.use(async (req, res, next) => {
 app.get('/', (req, res)=> {
     res.send("API WORKING")
 })
+app.use("/api/user", userRouter)
 
 
 if (process.env.VERCEL !== '1') {
